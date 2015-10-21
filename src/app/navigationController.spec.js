c45a71f4faae8f6111c290acca5f6e8b48ed1094
chrisleedavis <chrisleedavis@gmail.com>
2015-10-21 16:20:25 -0400

    Add code coverage to NavCtrl.isActive flag

--- src/app/navigationController.spec.js
+++ src/app/navigationController.spec.js
@@ -3,23 +3,25 @@
 
     var $rootScope,
         $location,
+        $routeParams,
         route;
 
     beforeEach(module("dtApp"));
 
     describe("Navigation Tests", function () {
 
-        beforeEach(inject(function (_$rootScope_, _$location_, $controller) {
+        beforeEach(inject(function (_$rootScope_, _$location_, _$routeParams_, $controller) {
 
             $rootScope = _$rootScope_;
             $location = _$location_;
+            $routeParams = _$routeParams_;
 
             //mock path functionality for tests
             $location.path = function(path) {
                 route = "#" + path;
             };
 
-            $controller("NavCtrl", { $scope: $rootScope, $location: $location });
+            $controller("NavCtrl", { $scope: $rootScope, $location: $location, $routeParams: $routeParams });
 
         }));
 
@@ -30,6 +32,15 @@
             $rootScope.redirect(path);
             expect(route).toEqual("#" + path);
 
+        });
+
+        it("will set isActive when active", function() {
+
+            var page = "blah";
+
+            $routeParams.page = page;
+            expect($rootScope.isActive(page)).toBeTruthy();
+            expect($rootScope.isActive("foo")).not.toBeTruthy();
         });
 
     });
