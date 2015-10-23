6772f1a3feede5ea15467e39bfd138da5d94449a
chrisleedavis <chrisleedavis@gmail.com>
2015-10-23 14:43:46 -0400

    Add initial functionality for creating an employee record with the server (minus DB).

--- src/app/clients/clientsController.spec.js
+++ src/app/clients/clientsController.spec.js
@@ -1,8 +1,19 @@
 (function () {
     "use strict";
 
-    var $controller,
-        $rootScope;
+    var $rootScope,
+        queue,
+        raised,
+        setupForm = function() {
+            return {
+                $invalid: true,
+                $error: [[{ $setTouched: function() { queue.push(raised); }}]]
+            };
+        },
+        assertTouchEvent = function() {
+            expect(queue.length).toEqual(1);
+            expect(queue[0]).toEqual(raised);
+        };
 
     beforeEach(module("dtApp"));
 
@@ -10,15 +21,88 @@
 
         beforeEach(inject(function (_$controller_, _$rootScope_) {
 
+            var mockEmployeeModel = {
+                    saveEmployee: function (employee) {
+                        return {
+                            then: function (fn) {
+                                employee.fromServer = true;
+                                fn(employee);
+                            }
+                        };
+                    }
+                };
+
             $rootScope = _$rootScope_;
-            $controller = _$controller_;
-            $controller("ClientsCtrl", { $scope: $rootScope });
+            _$controller_("ClientsCtrl", { $scope: $rootScope, dtEmployeeModel: mockEmployeeModel });
+
+            queue = [];
+            raised = "raised";
+
+            //mock FileReader
+            FileReader = function() {
+
+                return this;
+            };
+
+            FileReader.prototype.onload = angular.noop;
+            FileReader.prototype.readAsDataURL = function (file) {
+
+                var e = {
+                    target: {
+                        result: file
+                    }
+                };
+
+                this.onload(e);
+            };
 
         }));
 
-        it("message set successfully", function () {
+        it("successfully raise touch event if employee form is invalid", function () {
 
-            expect($rootScope.message).toEqual("future home of Clients functionality");
+            $rootScope.employeeForm = setupForm();
+            $rootScope.saveEmployee();
+            assertTouchEvent();
+        });
+
+        it("successfully raise touch event if notify form is invalid", function () {
+
+            $rootScope.notificationForm = setupForm();
+            $rootScope.sendNotification();
+            assertTouchEvent();
+        });
+
+        it("successfully don't raise touch event if notify form is valid", function () {
+
+            $rootScope.notificationForm = {$invalid: false};
+            $rootScope.sendNotification();
+            expect(queue.length).toEqual(0);
+        });
+
+        it("successfully updates employee from server with saveEmployee call", function() {
+
+            var employee = { foo: "bar" };
+            $rootScope.employee = employee;
+            $rootScope.employeeForm = { $invalid: false };
+
+            expect($rootScope.employee.fromServer).not.toBeTruthy();
+            $rootScope.saveEmployee();
+            expect($rootScope.employee.fromServer).toBeTruthy();
+        });
+
+        it("should not set employee.image if there's no file present", function() {
+
+            var file = null;
+            $rootScope.uploadImage(file);
+            expect($rootScope.employee.image).toBeNull();
+
+        });
+
+        it("should set employee.image if file present", function() {
+
+            var file = "fooBar";
+            $rootScope.uploadImage(file);
+            expect($rootScope.employee.image).toEqual(file);
 
         });
 
