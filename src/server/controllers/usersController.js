c46726efb1add26bc905a6f34ce3a412b638c320
Chris Davis <chrisleedavis@gmail.com>
2016-06-21 13:13:46 -0400

    Fixing bad merge issues

--- src/server/controllers/usersController.js
+++ src/server/controllers/usersController.js
@@ -17,18 +17,10 @@ class UsersController extends BaseController {
         encryption.encryptText(user.password)
             .then((encryptedPassword) => {
                 user.password = encryptedPassword;
-<<<<<<< HEAD
                 user.save()
                     .then((u) => {
                         const encryptedToken = encryption.encryptToken(u);
-
                         super.handleResponse(response, encryptedToken);
-=======
-                user.createdBy = request.user._id;
-                user.save()
-                    .then((u) => {
-                        super.handleResponse(response, u);
->>>>>>> 6e9e0b22bfc1503dbbb8653dc253a5ee0b5478ba
                     })
                     .catch((err) => {
                         super.handleError(response, err);
