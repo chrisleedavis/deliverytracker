9fff5a964aecf7a37a23a986ef75b0ab4acfa9d1
Chris Davis <chrisleedavis@gmail.com>
2016-06-21 13:09:29 -0400

    Fixing bad merge issues

--- src/app/login/loginController.js
+++ src/app/login/loginController.js
@@ -2,13 +2,8 @@
     "use strict";
 
     angular.module("dtApp")
-<<<<<<< HEAD
         .controller("LoginCtrl", ["$scope", "$window", "$location", "dtLoginService", "dtEmitLoginModel", "$q",
             function($scope, $window, $location, loginService, emitLoginModel, $q) {
-=======
-        .controller("LoginCtrl", ["$scope", "$window", "$location", "dtLoginService", "$q",
-            function($scope, $window, $location, loginService, $q) {
->>>>>>> 6e9e0b22bfc1503dbbb8653dc253a5ee0b5478ba
 
                 var setupLogin = function() {
                     $scope.login = {
@@ -21,15 +16,10 @@
                     var defer = $q.defer();
 
                     loginService.save($scope.login, function(data) {
-<<<<<<< HEAD
-                        
                         emitLoginModel.emitLogin($scope, data);
-                        
-=======
                         $window.sessionStorage.token = data.d;
                         $location.path("/");
                         $scope.$emit("login");
->>>>>>> 6e9e0b22bfc1503dbbb8653dc253a5ee0b5478ba
                         defer.resolve();
                     });
 
